eb79c884393e06aa45a673837882c88c1a8be1aa
rinben <bkrinald@buffalo.edu>
2025-08-04 21:01:35 -0400

    feat: created About the Couple component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,11 @@
+// src/App.jsx
+import About from './components/About';
+
 function App() {
   return (
     <div>
       <h1>Hello, Wedding Website!</h1>
+      <About />
     </div>
   );
 }
---
+++ client/src/components/About.jsx
@@ -0,0 +1,13 @@
+// src/components/About.jsx
+import React from 'react';
+
+function About() {
+  return (
+    <section>
+      <h2>About the Couple</h2>
+      <p>This is where you'll tell your love story! Talk about how you met, some of your favorite memories together, and what you're excited for in the future.</p>
+    </section>
+  );
+}
+
+export default About;
