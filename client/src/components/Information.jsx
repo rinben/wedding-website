38ba6124206f2dc4f2440fac8925971ad237e141
rinben <bkrinald@buffalo.edu>
2025-08-23 15:21:01 -0400

    updated information

--- client/src/components/Information.jsx
+++ client/src/components/Information.jsx
@@ -4,19 +4,21 @@ import "./Information.css";
 
 function Information() {
   return (
-    <section className="information-section">
+    <section id="information" className="information-section">
       <h2>Important Information</h2>
-      <div className="info-card">
-        <h3>Venue</h3>
-        <p>Bay Beach Club, Ontario, Canada</p>
-      </div>
-      <div className="info-card">
-        <h3>Date</h3>
-        <p>September 12, 2026</p>
-      </div>
-      <div className="info-card">
-        <h3>Attire</h3>
-        <p>Semi-formal colorful</p>
+      <div className="info-card-container">
+        <div className="info-card">
+          <h3>Venue</h3>
+          <p>Bay Beach Club, Ontario, Canada</p>
+        </div>
+        <div className="info-card">
+          <h3>Date</h3>
+          <p>September 12, 2026</p>
+        </div>
+        <div className="info-card">
+          <h3>Attire</h3>
+          <p>Semi-formal colorful</p>
+        </div>
       </div>
     </section>
   );
